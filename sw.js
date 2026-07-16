5abec3033729ebf038cfd520887909d2310a054c
houda789907-Learn <houda789907@gmail.com>
2026-07-16 03:05:31 +0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,22 @@
+const cacheName = 'sayed-store-v1';
+const assets = [
+  '/',
+  '/index.html',
+  '/manifest.json'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(cacheName).then(cache => {
+      cache.addAll(assets);
+    })
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cachedResponse => {
+      return cachedResponse || fetch(e.request);
+    })
+  );
+});
